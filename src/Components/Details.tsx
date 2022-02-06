776e5eff84a2d34b872b4a673634c9c41c2a95e0
정형진 <pnescene@icloud.com>
2022-02-06 21:30:32 +0900

    디테일 페이지 구조 설계

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,5 +1,9 @@
+import styled from "styled-components";
+
+const DetailsHeading = styled.h1``;
+
 function Details() {
-  return null;
+  return <DetailsHeading>차원이 다른 디테일</DetailsHeading>;
 }
 
 export default Details;

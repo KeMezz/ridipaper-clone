8956397a431b90089f694df9348ad9eed43864b4
정형진 <pnescene@icloud.com>
2022-02-06 21:30:38 +0900

    구조 페이지 구조설계

--- src/Components/Structure.tsx
+++ src/Components/Structure.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
+const HeadingText = styled.div`
+  text-align: center;
+`;
+const StrucGrid = styled.div``;
+const TextBox = styled.div``;
+
 function Structure() {
-  return null;
+  return (
+    <>
+      <HeadingText>완벽한 독서 경험</HeadingText>
+      <StrucGrid>
+        <TextBox></TextBox>
+        <TextBox></TextBox>
+      </StrucGrid>
+    </>
+  );
 }
 
 export default Structure;
